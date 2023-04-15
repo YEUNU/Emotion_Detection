3d5b994f7adf354cd16242444fe47d4184237c76
YEUNU <grinteacake@naver.com>
2023-04-15 22:18:13 +0900

    init_page_add

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flet import Text
 from glob import glob
 import os
 
-class start_page(UserControl):
+class password_page(UserControl):
     def __init__(self):
         pass
 
@@ -15,9 +15,13 @@ class main_page(UserControl):
 def init_system():
     path = os.getcwd()
     os.chdir(path)
-    temp = glob(os.path.join(*["src","fonts","*.ttf"]))
+    
+    src = {}
+
     fonts = {}
-    for i in temp:
+    imgs = {}
+
+    for i in glob(os.path.join(*["src","fonts","*.ttf"])):
         if "NanumGothicBold" in i:
             fonts["bold"] = i
         elif "NanumGothicExtraBold" in i:
@@ -26,26 +30,39 @@ def init_system():
             fonts["light"] = i
         elif "NanumGothic" in i:
             fonts["normal"] = i
-    return fonts
+    
+    for i in glob(os.path.join(*["src","img","*.svg"])):
+        if "background" in i:
+            imgs["background"] = i
+
+    src["fonts"] = fonts
+    src["imgs"] = imgs
+
+    return src
 
 def init_flet(page : Page):
-    fonts = init_system()
+    page.title = "My Little Friend"
+    page.bgcolor = ft.colors.LIGHT_BLUE_100
+    src = init_system()
     page.fonts = {
-        "nanum_normal" : fonts["normal"],
-        "nanum_bold" : fonts["bold"],
-        "nanum_extrabold" : fonts["extrabold"],
-        "nanum_light" : fonts["light"],
+        "nanum_normal" : src["fonts"]["normal"],
+        "nanum_bold" : src["fonts"]["bold"],
+        "nanum_extrabold" : src["fonts"]["extrabold"],
+        "nanum_light" : src["fonts"]["light"],
     }
-    t = Text(value="안녕하세요!", color="green",font_family="bold")
-    
-    page.title = "my first flet"
-    # t = Text(value="Hello, world!", color="green",font_family = )
-    # page.add(t)
-    page.controls.append(t)
-    page.update()
 
+    page.vertical_alignment = "center"
+    user_name = ft.TextField(label="이름",hint_text = "이름을 작성해주세요",text_size=15)
+    page.add(ft.Column(
+        [
+        ft.Row([ft.Icon(name = ft.icons.QUESTION_ANSWER,color = "white",size = 150)],alignment="center"),
+        ft.Row([ft.Text(value="편하게 이야기 하는", color="white",font_family="nanum_light",size=30,text_align="center")],alignment="center"),
+        ft.Row([ft.Text(value="나만의 작은 친구", color="white",font_family="nanum_extrabold",size=50,text_align="center")],alignment="center"),
+        ft.Row([user_name],alignment="center"),
+        
+        ]
+    ,spacing=25))
+    # page.add(ft.Text("hi"))
 
 ft.app(target=init_flet)
-
-
 print(init_system())
