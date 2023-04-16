35e709b32a90e0a2c1bd9ab4ce8035751384ebb8
YEUNU <grinteacake@naver.com>
2023-04-16 13:09:44 +0900

    view_added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flet as ft
-from flet import UserControl, Page
+from flet import KeyboardEvent, Page
 from flet import Text
 from glob import glob
 import os,sys
@@ -10,18 +10,7 @@ try:
 except:
     os.chdir(os.getcwd())
 
-class password_page(UserControl):
-    def __init__(self):
-        pass
-
-class main_page(UserControl):
-    def __init__(self):
-        pass
-
 def init_system():
-    # path = os.getcwd()
-    # os.chdir(path)
-    
     assets = {}
 
     fonts = {}
@@ -45,28 +34,70 @@ def init_system():
 
     return assets
 
-def init_flet(page : Page):
-    page.title = "My Little Friend"
-    page.bgcolor = ft.colors.LIGHT_BLUE_100
+def main(page : Page):
     assets = init_system()
-    page.fonts = {
-        "nanum_normal" : assets["fonts"]["normal"],
-        "nanum_bold" : assets["fonts"]["bold"],
-        "nanum_extrabold" : assets["fonts"]["extrabold"],
-        "nanum_light" : assets["fonts"]["light"],
-    }
+    def style_init():
+        page.title = "My Little Friend"
+        page.window_height=800
+        page.window_width=480
+        page.window_min_height = 800
+        page.window_min_width=480
+        page.fonts = {
+            "nanum_normal" : assets["fonts"]["normal"],
+            "nanum_bold" : assets["fonts"]["bold"],
+            "nanum_extrabold" : assets["fonts"]["extrabold"],
+            "nanum_light" : assets["fonts"]["light"],
+        }
+    style_init()
 
-    page.vertical_alignment = "center"
+    # page.window_center()
     user_name = ft.TextField(label="이름",hint_text = "이름을 작성해주세요",text_size=15)
-    page.add(ft.Column(
-        [
-        ft.Row([ft.Icon(name = ft.icons.QUESTION_ANSWER,color = "white",size = 150)],alignment="center"),
-        ft.Row([ft.Text(value="편하게 이야기 하는", color="white",font_family="nanum_light",size=30,text_align="center")],alignment="center"),
-        ft.Row([ft.Text(value="나만의 작은 친구", color="white",font_family="nanum_extrabold",size=50,text_align="center")],alignment="center"),
-        ft.Row([user_name],alignment="center"),
-        
-        ]
-    ,spacing=25))
-    # page.add(ft.Text("hi"))
+    password = ft.TextField(label="비밀번호",hint_text = "비밀번호를 작성해주세요",text_size=15,password=True, can_reveal_password=True)
 
-ft.app(target=init_flet,assets_dir="assets")
+    main_page = ft.View(
+                "/",
+                [
+                    ft.Icon(name = ft.icons.QUESTION_ANSWER,color = "black",size = 150),
+                    ft.Text(value="편하게 이야기 하는", color="black",font_family="nanum_light",size=30,text_align="center"),
+                    ft.Text(value="나만의 작은 친구", color="black",font_family="nanum_extrabold",size=50,text_align="center"),
+                    user_name,
+                    ft.ElevatedButton("확인", on_click=lambda _: page.go("/password")),
+                ],spacing=25,vertical_alignment="center",horizontal_alignment="center"
+            )
+    password_page =ft.View(
+                    "/password",
+                    [
+                        ft.Icon(name = ft.icons.PASSWORD,color = "black",size = 150),
+                        password,
+
+                    ],spacing=25,vertical_alignment="center",horizontal_alignment="center"
+                )
+    
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            main_page
+            
+        )
+        if page.route == "/password":
+            page.views.append(
+                password_page
+            )
+        page.update()
+
+    def on_keyboard(e: KeyboardEvent):
+        if e.key =="Enter" and page.route == "/" and user_name.value != "":
+            page.go("/password")
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_keyboard_event = on_keyboard
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
+
+ft.app(target=main,assets_dir="assets")
+
